c2f0c32a7be160a7d3a959d4627649043ba44c20
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-10-25 08:51:30 +0100

    update to include latest commits

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,11 @@
+// width functions
+function getWidth(){
+  return window.innerWidth
+}
+
+function BiggerThen991() {
+  return getWidth() >= 992
+}
 
 //register service worker
 
@@ -11,6 +19,15 @@ if ('serviceWorker' in navigator) {
 
 //side bar
 var SidebarInstances = M.Sidenav.init(document.querySelectorAll('.sidenav'), {"edge": "right"});
+document.querySelectorAll(".closeSidenavs").forEach((elem) => {
+  elem.onclick = () => {
+    if (!BiggerThen991()) {
+      for ( let _ = 0; _ <= SidebarInstances.length ; _++ ) {
+        SidebarInstances[_].close();
+      }
+    }
+  }
+})
 //collapsable
 var CollapsableInstances = M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
 //sort buttons
