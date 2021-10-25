2f33e222a22c524d8cdaed87d47858071e97c2dd
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-10-25 09:01:25 +0100

    improve indentation on script

--- assets/js/animations/barba.js
+++ assets/js/animations/barba.js
@@ -1,64 +1,63 @@
 //import {addScrollTrigger} from './SlideOnVeiwportEnter.js';
 
-  barba.init({
-    chacheIgnore: true,//becuase we have registered service worker we do not need to chache twice
-    transitions: [
-      //home transitions
-      {
-        from: {namespace: "home"},
-        leave: (data) =>
-          gsap.to(data.current.container, {
-            opacity: 0,
-            x: -10,
-            duration: 0.4
-          }),
-        afterLeave: (data) =>
-          gsap.to(data.current.container, {
-            //hide an element so it is'nt even rendered in the veiwport
-            display: "none",
-            duration: 0
-          }),
-        enter: (data) =>
-          gsap.from(data.next.container, {
-            opacity: 0,
-            x: 10,
-            duration: 0.4
-          })
-      },//from
-      {
-        to: {namespace: "home"},
-        //sync: true,
-        leave: (data) =>
-          gsap.to(data.current.container, {
-            opacity: 0,
-            x: 10,
-            duration: 0.4
-          }),
-        afterLeave: (data) =>
-          gsap.to(data.current.container, {
-            //hide an element so it is'nt even rendered in the veiwport
-            display: "none",
-            duration: 0
-          }),
-        enter: (data) =>
-          gsap.from(data.next.container, {
-            opacity: 0,
-            x: -10,
-            duration: 0.4
-          })
-      },//to
-      
-      //other transitions
-      /*{
-        //this brakes transition
-        once: ({ next }) => gsap.from(next.container, {
-            opacity: 0,
-            y: 10,
-            duration: 0.4
+barba.init({
+  transitions: [
+    //home transitions
+    {
+      from: {namespace: "home"},
+      leave: (data) =>
+        gsap.to(data.current.container, {
+          opacity: 0,
+          x: -10,
+          duration: 0.4
         }),
-      },*/
-      {
-        //after: addScrollTrigger,//this function is from ./page first load animation see line 1
-      }
-    ]
-  });
+      afterLeave: (data) =>
+        gsap.to(data.current.container, {
+          //hide an element so it is'nt even rendered in the veiwport
+          display: "none",
+          duration: 0
+        }),
+      enter: (data) =>
+        gsap.from(data.next.container, {
+          opacity: 0,
+          x: 10,
+          duration: 0.4
+        })
+    },//from
+    {
+      to: {namespace: "home"},
+      //sync: true,
+      leave: (data) =>
+        gsap.to(data.current.container, {
+          opacity: 0,
+          x: 10,
+          duration: 0.4
+        }),
+      afterLeave: (data) =>
+        gsap.to(data.current.container, {
+          //hide an element so it is'nt even rendered in the veiwport
+          display: "none",
+          duration: 0
+        }),
+      enter: (data) =>
+        gsap.from(data.next.container, {
+          opacity: 0,
+          x: -10,
+          duration: 0.4
+        })
+    },//to
+    
+    //other transitions
+    /*{
+      //this brakes transition
+      once: ({ next }) => gsap.from(next.container, {
+          opacity: 0,
+          y: 10,
+          duration: 0.4
+      }),
+    },*/
+    {
+      //after: addScrollTrigger,//this function is from ./page first load animation see line 1
+    }
+  ]
+});
