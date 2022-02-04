5a21711b8968296aa15f23b610c0c943c861b77a
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-02-04 18:52:35 +0000

    refactor service worker

---
+++ assets/sw.js
@@ -0,0 +1,50 @@
+const cacheName = "pwa-cache-v2";
+
+const assets = [
+  'fallback_offline',
+];
+
+// install event
+self.addEventListener('install', evt => {
+  //cache files
+  evt.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate evenet
+self.addEventListener('activate', evt => {
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== cacheName)
+          .map(key => caches.delete(key))
+      )
+    })
+  );
+});
+
+const getFromCache = (cache, request, fallback) =>
+  cache.match(request).then((response) => response || fallback)
+
+const loadAndAddToCache = (request, cache) =>
+  fetch(request).then((response) => {
+    cache.put(request, response.clone());//add the response to the cache
+    return response || getFromCache(cache, "fallback_offline", "We really are'nt able to get that right now!") ;//return the response or the fallback offline page if that returns a 404
+  });
+
+const loadFromCache = (request, cache) =>
+  getFromCache(cache, request, loadAndAddToCache(event, cache))//return the reponse from the cache or the fetch page if it is not in the cache
+
+const loadData = (request) =>
+  caches.open(cacheName).then( (cache) => loadFromCache(request, cache) )
+
+//fetch event
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    loadData(event.request)
+  );
+});
