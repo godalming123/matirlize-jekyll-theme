4c18dc041ebb2a8edce2a5d8598592eff25a66ce
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-03-26 07:26:03 +0000

    update service worker so it actually fetches fallback page

--- sw.js
+++ sw.js
@@ -31,13 +31,13 @@ self.addEventListener('activate', evt => {
 });
 
 const getFromCache = (cache, request, fallback) =>
-  cache.match(request).then((response) => response || fallback)
+  cache.match(request).then((response) => response).catch(() => fallback)
 
 const loadAndAddToCache = (request, cache) =>
   fetch(request).then((response) => {
     cache.put(request, response.clone());//add the response to the cache
-    return response || getFromCache(cache, '{{ "/fallback_offline" | relative_url }}', "We really are'nt able to get that right now!") ;//return the response or the fallback offline page if that returns a 404 or a last resort error text if that fails
-  });
+    return response;//return the response (will use catch to return a fallback if it fails)
+  }).catch(() => getFromCache(cache, '{{ "/fallback_offline" | relative_url }}', "We really are'nt able to get that right now!"));//return the fallback offline page if that returns a 404 or a last resort error text if that fails
 
 const loadFromCache = (request, cache) =>
   getFromCache(cache, request, loadAndAddToCache(request, cache))//return the reponse from the cache or the fetch page if it is not in the cache
