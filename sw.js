44e6703be6135d50affca9a31ac50b13a0f849a6
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-03-26 11:09:38 +0000

    make service worker only fetch the data if it is not inthe cache

--- sw.js
+++ sw.js
@@ -7,6 +7,37 @@ const assets = [
   '{{ "/fallback_offline" | relative_url }}',
 ];
 
+const getFromCaches = (request, getFallback, getFallbackOnFailiure) =>
+  caches.match(request).then((response) => {
+    if (response != null) {//if we have a response
+      return response
+    }
+    else {//if the data is not in the cache
+      return getFallback()
+    }
+  }).catch((respone) => getfallbackOnFailiure())// catch with the fallback upon failure
+
+const loadAndAddToCache = (request) =>
+  fetch(request).then((response) => {
+    caches.open(cacheName).then( (cache) => cache.put(request, response.clone()));//add the response to the cache
+    return response;//return the response
+  })
+
+const loadData = (request, isHtml) =>
+  // look in all our caches for the response and load + add to cache as a fallback + load fallback page if that fails
+  getFromCaches(
+    request,
+    () => loadAndAddToCache(request),
+    () => {
+      if (isHtml) {
+        return getFromCaches('{{ "/fallback_offline" | relative_url }}', () => "the cached fallbck offline page was null", "getting the cached falback offline page failed")
+      }
+      else {
+        return ""
+      }
+    }
+  )
+
 // install event
 self.addEventListener('install', evt => {
   //cache files
@@ -30,25 +61,9 @@ self.addEventListener('activate', evt => {
   );
 });
 
-const getFromCaches = (request, fallback, fallbackOnFailiure) =>
-  caches.match(request).then((response) => response || fallback).catch((respone) => fallbackOnFailiure)// get the content from the cache and catch with the fallback upon failure
-
-const loadAndAddToCache = (request) =>
-  fetch(request).then((response) => {
-    caches.open(cacheName).then( (cache) => cache.put(request, response.clone()));//add the response to the cache
-    return response;//return the response (will use catch to return a fallback if it fails)
-  })
-
-const loadData = (request) =>
-  // look in all our caches for the response and load + add to cache as a fallback + load fallback page if that fails
-  getFromCaches(
-      request,
-      loadAndAddToCache(request),
-      getFromCaches('{{ "/fallback_offline" | relative_url }}', "We really are'nt able to get that right now!"))
-
 //fetch event
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    loadData(event.request)
+    loadData(event.request, (event.request.url.indexOf('.html') > -1))
   );
 });
